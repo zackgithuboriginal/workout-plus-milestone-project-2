952b842f053954073660d9c9b66b455b3afd0f27
zackgithuboriginal <z.gibbons@hotmail.com>
2020-11-05 16:49:13 +0000

    Added ability to take users location using HTML geolocation API and center the map on that location

--- assets/js/gym_finder.js
+++ assets/js/gym_finder.js
@@ -1,11 +1,22 @@
 let map;
 let mapLocation;
 
-function initMap() {
-    mapLocation =  {lat: 51.8985, lng: -8.4756};
+function initMap(userLocation) {
+    if (userLocation){
+       mapLocation =  {lat: userLocation.coords.latitude, lng: userLocation.coords.longitude};
+    } else {
+        mapLocation =  {lat: 51.8985, lng: -81.4756};
+    }
     map = new google.maps.Map(document.getElementById("map"), {
         center: mapLocation,
         zoom: 10,
     });
 };
 
+function getGeoLocation() {
+    if(navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(initMap);
+    } else {
+        console.log("nope")
+    } 
+}
