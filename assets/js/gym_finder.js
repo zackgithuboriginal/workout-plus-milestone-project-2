d274d1e5ef486622f029b4fc959b9cd5d4508a23
zackgithuboriginal <z.gibbons@hotmail.com>
2020-11-05 15:56:08 +0000

    Adjusted initMap function to initialise variables outside of function to increase scope

--- assets/js/gym_finder.js
+++ assets/js/gym_finder.js
@@ -1,6 +1,11 @@
+let map;
+let mapLocation;
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 51.8985, lng: -8.4756},
+    mapLocation =  {lat: 51.8985, lng: -8.4756};
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: mapLocation,
         zoom: 10,
     });
 };
+
