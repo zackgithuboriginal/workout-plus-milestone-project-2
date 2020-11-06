1b880ebd7491fa3daab548f9e3f4b8cc296091e2
zackgithuboriginal <z.gibbons@hotmail.com>
2020-11-06 13:35:00 +0000

    Added findLocalGyms function that creates an object to contain gyms around user's location

--- assets/js/gym_finder.js
+++ assets/js/gym_finder.js
@@ -1,18 +1,6 @@
 let map;
 let mapLocation;
 
-function initMap(userLocation) {
-    if (userLocation){
-       mapLocation =  {lat: userLocation.coords.latitude, lng: userLocation.coords.longitude};
-    } else {
-        mapLocation =  {lat: 51.8985, lng: -81.4756};
-    }
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: mapLocation,
-        zoom: 10,
-    });
-};
-
 function getGeoLocation() {
     if(navigator.geolocation) {
        navigator.geolocation.getCurrentPosition(initMap);
@@ -20,3 +8,27 @@ function getGeoLocation() {
         console.log("nope")
     } 
 }
+
+function initMap(userLocation) {
+    if (userLocation){
+        mapLocation =  {lat: userLocation.coords.latitude, lng: userLocation.coords.longitude};
+    } else {
+        mapLocation =  {lat: 51.8985, lng: -81.4756};
+    }
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: mapLocation,
+        zoom: 10,
+    });
+
+    findLocalGyms(mapLocation);
+};
+
+
+
+function findLocalGyms(userLocation) {
+    let request = {
+        location: userLocation,
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        keyword: 'gym'
+    };
+};
