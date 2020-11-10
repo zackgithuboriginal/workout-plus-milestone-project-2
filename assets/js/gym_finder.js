1502283c26e786ada3bcb0f4f6a0540e1eea5d1c
zackgithuboriginal <z.gibbons@hotmail.com>
2020-11-10 14:28:26 +0000

    Small change to padding of map element and changed default location of map

--- assets/js/gym_finder.js
+++ assets/js/gym_finder.js
@@ -21,7 +21,7 @@ function initMap(userLocation) {
     if (userLocation){
         mapLocation =  {lat: userLocation.coords.latitude, lng: userLocation.coords.longitude};
     } else {
-        mapLocation =  {lat: 51.8985, lng: -81.4756};
+        mapLocation =  {lat: 53.3498, lng: -6.2603};
     }
     map = new google.maps.Map(document.getElementById("map"), {
         center: mapLocation,
