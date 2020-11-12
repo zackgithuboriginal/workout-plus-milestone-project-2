7b981596ee93904a9a5e3ae82aa33c163b32ed59
zackgithuboriginal <z.gibbons@hotmail.com>
2020-11-12 12:30:28 +0000

    Added autocomplete functionality on address input box

--- assets/js/gym_finder.js
+++ assets/js/gym_finder.js
@@ -6,6 +6,7 @@ let service;
 let infoWindow;
 let currentInfoWindow;
 let geocoder;
+let options;
 
 // Handles the initial map creation
 function initMap() {
@@ -18,6 +19,22 @@ function initMap() {
         zoom: 15,
     });
 
+
+// Handles autocomplete requests on address input
+    let input = document.getElementById("address-input");
+    let autocomplete = new google.maps.places.Autocomplete(input, options);
+    options = {
+        types: ['address']
+    }
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            let place=autocomplete.getPlace();
+            if (!place.geometry){
+                return;
+            }
+            else if (place.geometry) {
+                repositionMap(place.geometry.location) 
+            }
+        });
     findLocalGyms(mapLocation);
 };
 
